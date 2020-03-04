449dd7c80cffe676206fef5ef6ccfd1bef47ccb3
Michael Brubaker <mb997315@ohio.edu>
2020-03-04 10:37:06 -0500

    Changes

--- DAL/MIS4200Team1Context.cs
+++ DAL/MIS4200Team1Context.cs
@@ -5,12 +5,4 @@ using System.Web;
 using System.Data.Entity;
 
 namespace Team1Centric940.DAL
-{
-    public class MIS4200Team1Context : DBContext
-    {
-        public  MIS4200Team1Context() : base ("name=DefaultConnection")
-            {
-
-            }
-    }
-}
+{ }
