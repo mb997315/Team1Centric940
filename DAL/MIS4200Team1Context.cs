816dad44e72979dc30a2082dd50f0780ac751e07
Michael Brubaker <mb997315@ohio.edu>
2020-03-04 10:35:42 -0500

    New Changes

---
+++ DAL/MIS4200Team1Context.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Team1Centric940.DAL
+{
+    public class MIS4200Team1Context : DBContext
+    {
+        public  MIS4200Team1Context() : base ("name=DefaultConnection")
+            {
+
+            }
+    }
+}
---
+++ Models/Gratitude.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Team1Centric940.Models
+{
+    public class Gratitude
+    {
+        public int gratitudeId { get; set; }
+        public string firstName { get; set; }
+        
+        public DateTime employerSince { get; set; }
+
+    }
+}
